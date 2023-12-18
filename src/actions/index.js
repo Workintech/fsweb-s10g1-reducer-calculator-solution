82712dd491081b9d81b2a4bb1c9bc713b90019f9
Gökhan <gn.ozdemir@gmail.com>
2023-12-18 16:24:24 +0300

    reducer initial setup

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,17 @@ export const ADD_ONE = "ADD_ONE";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
 
+export const GOKHAN = "GÖKHAN ÖZDEMİR";
+
+// ENUM nedir?
+// https://www.tutorialspoint.com/what-is-enum-in-javascript
+export const L = "Omuz genişliği 80cm, bel 70cm olan kalıp";
+export const M = "Omuz genişliği 70cm, bel 58cm olan kalıp";
+
 export const addOne = () => {
-  return ({ type: ADD_ONE });
-}
+  return { type: ADD_ONE };
+};
 
 export const applyNumber = (number) => {
-  return ({ type: APPLY_NUMBER, payload: number });
-}
+  return { type: APPLY_NUMBER, payload: number };
+};
